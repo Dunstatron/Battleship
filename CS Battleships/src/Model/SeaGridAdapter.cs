3076c3751f24ec5a2cae7d1d52db8ff9db553192
101626449 <101626449@student.swin.edu.au>
2018-04-12 15:35:16 +1000

    fix SeaGridAdapter

--- CS Battleships/src/Model/SeaGridAdapter.cs
+++ CS Battleships/src/Model/SeaGridAdapter.cs
@@ -33,20 +33,17 @@ namespace Battleship
         // '' <param name="x">tile x coordinate</param>
         // '' <param name="y">tile y coordinate</param>
         // '' <returns>a tile, either what it actually is, or if it was a ship then return a sea tile</returns>
-        public TileView this[int x, int y]
+        public TileView Item(int x, int y)
         {
-            get
-            {
-                TileView result = _MyGrid.Item(x, y);
+            TileView result = _MyGrid.Item(x, y);
 
-                if (result == TileView.Ship)
-                {
-                    return TileView.Sea;
-                }
-                else
-                {
-                    return result;
-                }
+            if (result == TileView.Ship)
+            {
+                return TileView.Sea;
+            }
+            else
+            {
+                return result;
             }
         }
 
