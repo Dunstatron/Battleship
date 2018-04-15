3abf084273e3d50b4f2770a0176bfaa8ab1ee2c7
101626449@student.swin.edu.au <101626449@student.swin.edu.au>
2018-04-15 13:29:35 +1000

    fix highscore not saving

--- CS Battleships/src/HighScoreController.cs
+++ CS Battleships/src/HighScoreController.cs
@@ -196,6 +196,7 @@ namespace Battleship
                 _Scores.RemoveAt((_Scores.Count - 1));
                 _Scores.Add(s);
                 _Scores.Sort();
+                SaveScores();
                 GameController.EndCurrentState();
             }
 
